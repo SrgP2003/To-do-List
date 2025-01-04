dddfc37f899059edc97b813f97c2bc4052176d83
Sergio Palacios <sergioarepalacios9264@gmail.com>
2025-01-03 20:37:43 -0600

    Modificaciones adicionales en el diseño e implementación de lógica principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/styles.css";
-import Task from './components/TasksTools'
 import Content from './components/Content';
 
 export default function App(){
   return(
     <>
-    <Content/>
-    <Task/> 
+    <Content/> 
     </>
   )
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,25 @@
 import "../styles/Content.css";
 import TaskChart from "./TasksTools";
+import "../styles/TaskChart.css";
+import { useState } from "react";
 
 export default function Content() {
+    const [tasks, setTasks] = useState(""); //Estado para capturar las tareas que se digiten en el textarea
+    const [isPriority, setIsPriority] = useState(false); //Estado para capturar si la tarea es prioridad o no
+    const [primaryTasks, setPrimaryTasks] = useState([]); //Estado para guardar las tareas con prioridad
+    const [secondaryTasks, setSecondaryTasks] = useState([]); //Estado para guardar las tareas sin prioridad
+
+    const handleClickAddTask = () => { //Funcion que sera aplicada sobre el boton para agregar tareas
+        if (!tasks.length)
+            alert("Por favor, escriba una tarea");
+        if (isPriority)
+            setPrimaryTasks([...primaryTasks, tasks]);
+        else {
+            setSecondaryTasks([...secondaryTasks, tasks]);
+        }
+        setTasks("");
+    }
+
     return (
         <>
             <main className="main-content">
@@ -13,19 +31,50 @@ export default function Content() {
                 </header>
                 <section className="container">
                     <article className="row">
-                        <div className="col-12 d-flex justify-content-center">
-                            <textarea id="textarea-tasks" className="form-control m-3" placeholder="Escribe algo..."></textarea>
+                        <div className="col-12">
+                            <div className="div-lg-task-container">
+                                <textarea value={tasks} onChange={e => setTasks(e.target.value)} rows="5" id="textarea-tasks" className="form-control mb-2" placeholder="Escribe algo..."></textarea>
+                                <hr />
+                                <div className="row div-md-task-buttons">
+                                    <div className="col-sm-12 col-md-6 col-lg-6 div-md-button d-flex justify-content-center">
+                                        <button onClick={handleClickAddTask} className="btn btn-add">Agregar</button>
+                                    </div>
+                                    <div className="col-sm-12 col-md-6 col-lg-6 form-check-priority form-check form-switch d-flex align-items-center justify-content-center">
+                                        <div className="row">
+                                            <div className="col-3">
+                                                <input checked={isPriority} onChange={e => setIsPriority(e.target.checked)} title="Marcar actividad como prioridad" className="form-check-input" type="checkbox" role="switch" />
+                                            </div>
+                                            <div className="col-9 ">
+                                                <label className="form-check-label">PRIORIDAD</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <hr />
                     </article>
                     <article className="row">
                         <div className="col-md-12 col-lg-6">
-                            <TaskChart title={"Mis mayores prioridades"}>
-                                {/* Implementacion de tareas luego*/}
+                            <TaskChart title={"Mis mayores prioridades"}> {/* Se implementan las tareas con prioridad */}
+                                <ul>
+                                    {
+                                        primaryTasks.map((task, index) => (
+                                            <li key={index}>{task}</li>
+                                        ))
+                                    }
+                                </ul>
                             </TaskChart>
                         </div>
                         <div className="col-md-12 col-lg-6">
-                            <TaskChart title={"Sin prioridad pero necesarias"}>
-                                {/* Implementacion de tareas luego*/}
+                            <TaskChart title={"Sin prioridad pero necesarias"}> {/* Se implementan las tareas sin prioridad */}
+                                <ul>
+                                    {
+                                        secondaryTasks.map((task, index) => (
+                                            <li key={index}>{task}</li>
+                                        ))
+                                    }
+                                </ul>
                             </TaskChart>
                         </div>
                     </article>
